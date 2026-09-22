1d9be7854f19cc352a9cce0da4d62310789f679f
Kit Webster <kitwebster@gmail.com>
2026-09-22 16:36:25 +1000

    Replace September tickets with cancellation notice

--- commune-offer.js
+++ commune-offer.js
@@ -6,15 +6,9 @@
   };
 
   const update = () => {
-    setText("[data-offer-label]", "General admission");
-    setText("[data-offer-price]", "25");
-    setText("[data-offer-countdown]", "Thursday 24 September");
-    setText("[data-ticket-button-label]", "Get tickets");
-    setText(
-      "[data-event-pricing]",
-      "$25 general admission · $40 for two dancers"
-    );
-    document.body.dataset.offerState = "general-admission";
+    setText("[data-offer-countdown]", "NEXT DATE TO BE ANNOUNCED");
+    setText("[data-ticket-button-label]", "JOIN THE LIST");
+    document.body.dataset.offerState = "future-dates";
   };
 
   const installMobileTicketDock = () => {
